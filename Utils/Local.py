8db472f9e77d74a32217005443b905641427f596
hamidm317 <hamidm317@gmail.com>
2024-09-15 20:35:12 +0330

    Modify ActionReward Extractor to Get all Stims and RT Extractor

--- Utils/Local.py
+++ Utils/Local.py
@@ -18,27 +18,117 @@ def AvailableSubjects():
 
     return SOI
 
-def Local_ActionRewardExtractor(stim: int, dataframe_dir = r'E:\HWs\Msc\Research\Research\Depression Dataset\New Datasets\Subjects_Behavioral_datas.csv'):
+def ActionRewardExtractor(stim, dataframe_dir = r'E:\HWs\Msc\Research\Research\Depression Dataset\New Datasets\Subjects_Behavioral_datas.csv'):
+    
+    if type(stim) == int:
+
+        stim = [stim]
     
     dataframe = read_csv(dataframe_dir)
 
-    all_stim_action_list = []
-    all_stim_reward_list = []
+    all_stims_action_list = []
+    all_stims_reward_list = []
+    prob_classes = []
 
-    for sample in range(len(dataframe)):
+    if stim == 'All':
+
+        all_stim_action_list = []
+        all_stim_reward_list = []
+
+        for sample in range(len(dataframe)):
+            
+            list_ = ast.literal_eval(dataframe['Task'][sample])
+            Tasks_matrix = np.array(list_)
+
+            all_stim_action_list.append(np.squeeze(Tasks_matrix[:, 2]))
+            all_stim_reward_list.append((np.squeeze(Tasks_matrix[:, 3]) + 1) / 2)
+
+        prob_class = 0.7   
+
+
+        all_stims_action_list.append(all_stim_action_list)
+        all_stims_reward_list.append(all_stim_reward_list)
         
-        list_ = ast.literal_eval(dataframe['Task'][sample])
-        Tasks_matrix = np.array(list_)
+        prob_classes.append(prob_class)
 
-        all_stim_action_list.append(np.squeeze(Tasks_matrix[np.where(Tasks_matrix[:, 0] == stim), 2]))
-        all_stim_reward_list.append((np.squeeze(Tasks_matrix[np.where(Tasks_matrix[:, 0] == stim), 3]) + 1) / 2)
+    else:
 
-    prob_class = 0.8 - stim * 0.1
+        for stim_ in stim:
 
-    return all_stim_action_list, all_stim_reward_list, prob_class
+            all_stim_action_list = []
+            all_stim_reward_list = []
 
-def Local_Load_Cavanagh_EstParams(AvailableSubjects, excel_dir = r'E:\HWs\Msc\Research\Research\Depression Dataset\depression_rl_eeg\Depression PS Task\Scripts from Manuscript\Data_4_Import.xlsx'):
+            for sample in range(len(dataframe)):
+                
+                list_ = ast.literal_eval(dataframe['Task'][sample])
+                Tasks_matrix = np.array(list_)
+
+                all_stim_action_list.append(np.squeeze(Tasks_matrix[np.where(Tasks_matrix[:, 0] == stim_), 2]))
+                all_stim_reward_list.append((np.squeeze(Tasks_matrix[np.where(Tasks_matrix[:, 0] == stim_), 3]) + 1) / 2)
+
+            prob_class = 0.8 - stim_ * 0.1    
+
+
+            all_stims_action_list.append(all_stim_action_list)
+            all_stims_reward_list.append(all_stim_reward_list)
+            
+            prob_classes.append(prob_class)
+
+    return all_stims_action_list, all_stims_reward_list, prob_classes
+
+def LoadCavanaghEstParams(AvailableSubjects, excel_dir = r'E:\HWs\Msc\Research\Research\Depression Dataset\depression_rl_eeg\Depression PS Task\Scripts from Manuscript\Data_4_Import.xlsx'):
 
     tmp_Data = read_excel(excel_dir)
 
     return np.array(tmp_Data['TST_aG'][AvailableSubjects]), np.array(tmp_Data['TST_aL'][AvailableSubjects])
+
+def ReactionTimeExtractor(stim, dataframe_dir = r'E:\HWs\Msc\Research\Research\Depression Dataset\New Datasets\Subjects_Behavioral_datas.csv'):
+    
+    if type(stim) == int:
+
+        stim = [stim]
+    
+    dataframe = read_csv(dataframe_dir)
+
+    all_stims_rt_list = []
+    prob_classes = []
+
+    if stim == 'All':
+
+        all_stim_rt_list = []
+
+        for sample in range(len(dataframe)):
+            
+            list_ = ast.literal_eval(dataframe['Task'][sample])
+            Tasks_matrix = np.array(list_)
+
+            all_stim_rt_list.append(np.squeeze(Tasks_matrix[:, 1]))
+
+        prob_class = 0.7   
+
+
+        all_stims_rt_list.append(all_stim_rt_list)
+        
+        prob_classes.append(prob_class)
+
+    else:
+
+        for stim_ in stim:
+
+            all_stim_rt_list = []
+
+            for sample in range(len(dataframe)):
+                
+                list_ = ast.literal_eval(dataframe['Task'][sample])
+                Tasks_matrix = np.array(list_)
+
+                all_stim_rt_list.append(np.squeeze(Tasks_matrix[np.where(Tasks_matrix[:, 0] == stim_), 1]))
+
+            prob_class = 0.8 - stim_ * 0.1    
+
+
+            all_stims_rt_list.append(all_stim_rt_list)
+            
+            prob_classes.append(prob_class)
+
+    return all_stims_rt_list, prob_classes
